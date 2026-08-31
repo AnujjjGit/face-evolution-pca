c83324db528feec1668deca7282131989e72de59
AnujjjGit <147902439+AnujjjGit@users.noreply.github.com>
2026-08-30 22:18:17 -0400

    Initialize face PCA package

---
+++ src/face_pca/__init__.py
@@ -0,0 +1,5 @@
+"""Face image PCA representation analysis."""
+
+from face_pca.pipeline import fit_pca, load_image_matrix, reconstruction_mse
+
+__all__ = ["fit_pca", "load_image_matrix", "reconstruction_mse"]
